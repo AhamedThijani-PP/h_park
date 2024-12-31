fff116038a326a9c7969729f2c7df2ad10ad02cd
Ahamed Thijani PP <153520061+AhamedThijani-PP@users.noreply.github.com>
2024-12-31 15:43:53 +0530

    Delete menu directory

--- menu/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from . models import category,category_product,Slider
-
-# Register your models here.
-admin.site.register(category)
-admin.site.register(category_product)
-admin.site.register(Slider)
